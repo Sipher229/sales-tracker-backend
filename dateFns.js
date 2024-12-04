a07f5adc5bd100b887fa0a09d94e6859dc292dc7
Philippe Neri Singizwa <neriwest20@gmail.com>
2024-12-03 20:29:46 -0500

    add log to datefns to debug

--- dateFns.js
+++ dateFns.js
@@ -4,6 +4,7 @@ const getCurrentDateTme = () => {
     return new Date().toLocaleString('sv-SE') 
 }
 
+
 const getDifferenceInMinutes = (laterDate, earlierDate) => {
     return differenceInMinutes(laterDate, earlierDate)
 }
@@ -11,6 +12,7 @@ const getDifferenceInMinutes = (laterDate, earlierDate) => {
 const getCurrentDate = () => {
     return new Intl.DateTimeFormat('en-CA').format(new Date())
 }
-
+console.log(getCurrentDate())
+console.log(getCurrentDateTme())
 
 export {getCurrentDateTme, differenceInHours, getCurrentDate, getDifferenceInMinutes}
